eef2d105ae840ef6491f2bbf28f2d1f128727d06
savyjeremiah <saviourjeremiah2024@gmail.com>
2024-10-08 04:39:56 -0700

    first class1

---
+++ class1.py
@@ -0,0 +1,82 @@
+My_name = 'my name is saviour'
+age="56"
+gender ='male'
+number_of_student ='7'
+total ='39920'
+hobbies ="football"
+dream ='footballer'
+
+
+un1 = 10
+un2 = 20
+un3 = 30
+un4 = 40
+un1 = -10
+un2 = -20
+un3 = -30
+un4 = -40
+
+
+
+un1 =  90
+un2 = 34
+sum = un1 + un2
+print(sum)
+sum = un1 - un2
+print(sum)
+sum = un1 / un2
+print(sum)
+sum = un1 * un2
+print(sum)
+round =un1 // un2
+print(sum)
+round =un1 // un2
+print(sum)
+round =un1 // un2
+sum = un1 - un2
+sum = un1 + un2
+
+
+
+
+num1 =200
+num3 =300
+print(num3+num1)
+float_num =20.9 # number with decimal point
+print(float)
+
+string_value ="my name is saviour"
+print(string_value )
+
+s ="and jeremiah"
+
+print(s+string_value)
+  # mothod insting
+string_value.upper
+#print(string_value.islower())
+#print(string_value.replace("j","w"))  # for replace
+
+#print(string_value.lower())#
+#print(string_value.count()) # for counting
+
+
+ 
+
+#variablenam = input(prote)
+name = input("what is your name ?")
+print("your name is "+ name)
+
+
+
+# simple caluclator to grt addition and subtraction{}
+from math import*# inport all thre math functuon
+
+number1 = int(input(("enter any number")))
+number2= int(input(("enter another number")))
+sum = number1 + number2
+Sub = number2 - number1
+print(f" the same of the number are {sum}" )
+print(f" the subraction of the number is {Sub} ")
+
+print(sqrt(100))
+
